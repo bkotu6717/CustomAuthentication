9179493f055d336cdf9461740fe14c3d1fc4a233
Bhaskar <bhaskar.kotu@coupa.com>
2015-05-13 23:25:34 +0530

    Adding secret key base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+CustomAuthentication::Application.config.secret_token = '588a311ce25d73903d4fd7b2bf63acb0d809a57ed14084ea7af96c0d4597875bb3b0db5d71bec744bb2d073121a5c91b22ead94a351900ab6800738721956228'
